d1af947ef3ce977a740617bf3e48b1e8b52281f6
JHarvin <jeffethramos@gmail.com>
2019-01-04 16:07:24 -0600

    menu

---
+++ Sistema_Financiero/menu/menu.php
@@ -0,0 +1,82 @@
+<!--Menu-->
+<nav class="navbar navbar-light" style="background-color: #D0E8E3;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Articulos
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li>
+              <a href="../../modulos/articulos/index.php">Articulos</a>
+          </li>
+        <li>
+              <a href="../../modulos/inventario/index.php">Inventario</a>
+          </li>
+          <li>
+              <a href="../../modulos/inventario/stock.php">Baja Existencias</a>
+          </li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Ventas
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li>
+              <a href="../../modulos/ventas/index.php"> Consumidor Final</a>
+          </li>
+          <li>
+              <a href="../../modulos/ventas/credito.php">Crédito Fiscal</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+                  <a href="../../modulos/compras/index.php"><i class="fa fa-truck"></i> Compras</a>
+               </li>
+
+               <li>
+                   <a href="../../modulos/clientes/index.php"><i class="fa fa-user"></i> Clientes</a>
+               </li>
+
+               <li>
+                   <a href="../../modulos/proveedor/index.php"><i class="fa fa-users"></i> Proveedores</a>
+               </li>
+
+               <li>
+                   <a href="../../modulos/contable/index.php"><i class="fa fa-retweet"></i> Cobros y Pagos</a>
+               </li>
+               <li class="dropdown">
+                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Activo fijo
+                 <span class="caret"></span></a>
+                 <ul class="dropdown-menu">
+                   <li>
+                       <a href="../../modulos/reportes/cat.php"> Registro de Activo</a>
+                   </li>
+                    <li>
+                       <a href="../../modulos/reportes/report_costo.php"> Listado de Activos</a>
+                   </li>
+                   <li>
+                       <a href="../../modulos/reportes/dep.php"> Tipo de activo</a>
+                   </li>
+                   <li>
+                       <a href="../../modulos/reportes/compra_dia.php"> Encargado</a>
+                   </li>
+                    <li>
+                       <a href="../../modulos/reportes/compra_cat.php"> Institucion</a>
+                   </li>
+                   <li>
+                       <a href="../../modulos/reportes/compra_gen.php"> Departamento</a>
+                   </li>
+                 </ul>
+               </li>
+
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="fa fa-user"></span> Cerrar sesion</a></li>
+
+    </ul>
+  </div>
+</nav>
