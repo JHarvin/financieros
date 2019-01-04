0da6441c8567df8ffbb112a7140c196eb8bf42aa
JHarvin <jeffethramos@gmail.com>
2019-01-04 17:12:53 -0600

    correcion menu articulos falla despues de la primera vez

--- Sistema_Financiero/menu/menu.php
+++ Sistema_Financiero/menu/menu.php
@@ -48,31 +48,10 @@
                <li>
                    <a href="../../modulos/contable/index.php"><i class="fa fa-retweet"></i> Cobros y Pagos</a>
                </li>
-               <li class="dropdown">
-                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Activo fijo
-                 <span class="caret"></span></a>
-                 <ul class="dropdown-menu">
-                   <li>
-                       <a href="../../modulos/reportes/cat.php"> Registro de Activo</a>
-                   </li>
-                    <li>
-                       <a href="../../modulos/reportes/report_costo.php"> Listado de Activos</a>
-                   </li>
-                   <li>
-                       <a href="../../modulos/reportes/dep.php"> Tipo de activo</a>
-                   </li>
-                   <li>
-                       <a href="../../modulos/reportes/compra_dia.php"> Encargado</a>
-                   </li>
-                    <li>
-                       <a href="../../modulos/reportes/compra_cat.php"> Institucion</a>
-                   </li>
-                   <li>
-                       <a href="../../modulos/reportes/compra_gen.php"> Departamento</a>
-                   </li>
-                 </ul>
+              
+               <li>
+<a href="#" class="btn btn-danger">Ir a activo fijo</a>
                </li>
-
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="fa fa-user"></span> Cerrar sesion</a></li>
