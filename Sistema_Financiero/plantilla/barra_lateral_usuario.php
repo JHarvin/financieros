84222481ab72ccf2436c9f5782e68201d436dfe6
GersonDuran <32017747+GersonDuran@users.noreply.github.com>
2019-01-04 14:14:37 -0600

    t

---
+++ Sistema_Financiero/plantilla/barra_lateral_usuario.php
@@ -0,0 +1,146 @@
+<!-- Left Sidebar -->
+<aside id="leftsidebar" class="sidebar">
+    <!-- User Info -->
+    <div class="user-info">
+        <div class="image">
+            <img src="../images/asesor.png" width="48" height="48" alt="User" />
+        </div>
+        <div class="info-container">
+            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Asesor</div>
+            <div class="email">opciones</div>
+            <div class="btn-group user-helper-dropdown">
+                <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down </i>
+                <ul class="dropdown-menu pull-right">
+
+                    <li><a href="../index.php"><i class="material-icons">input</i>Salir</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- #User Info -->
+    <!-- Menu -->
+    <div class="menu">
+        <ul class="list">
+            <li class="header text-center">CUENTAS POR COBRAR</li>
+            <li>
+                <a href="../home/home_usuario.php">
+                    <i class="material-icons col-red">home</i>
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="menu-toggle">
+                    <i class="material-icons col-blue">collections</i>
+                    <span>Registro de Clientes</span>
+                </a>
+                <ul class="ml-menu">
+                    <li>
+                        <a href="../cuenta_cobrar/registro_natura.php">Persona Natural</a>
+                    </li>
+                    <li>
+                        <a href="../cuenta_cobrar/registro_juridico.php">Persona Juridica</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="menu-toggle">
+                    <i class="material-icons col-amber">description</i>
+                    <span>Registro de Credito</span>
+                </a>
+                <ul class="ml-menu">
+                    <li>
+                        <a href="../cuenta_cobrar/credito_personal.php">Personal</a>
+                    </li>
+
+                    <li>
+                        <a href="../cuenta_cobrar/credito_juridico.php">Empresaria</a>
+                    </li>
+                </ul>
+            </li>
+             <li>
+                <a href="#" class="menu-toggle">
+                    <i class="material-icons col-green">description</i>
+                    <span>Cartera de Clientes</span>
+                </a>
+                <ul class="ml-menu">
+                    <li>
+                        <a href="../cuenta_cobrar/cartera_normal.php">Normal</a>
+                    </li>
+
+                    <li>
+                        <a href="../cuenta_cobrar/cartera_mora.php">Mora</a>
+                    </li>
+                    <li>
+                        <a href="../cuenta_cobrar/cartera_incobrable.php">Incobrable</a>
+                    </li>
+                </ul>
+            </li>
+         <li>
+             <a href="../cuenta_cobrar/refinanciamiento.php" >
+                    <i class="material-icons col-red">assignment</i>
+                    <span>Refinanciamiento</span>
+                </a>
+            </li>
+            <li>
+                <a href="../cuenta_cobrar/estados_financieros.php" >
+                    <i class="material-icons col-deep-purple">assignment</i>
+                    <span>Estados Financieros</span>
+                </a>
+            </li>
+            <li>
+                <a href="../cuenta_cobrar/abono.php">
+                    <i class="material-icons col-deep-orange">book</i>
+                    <span>Registro de Abono</span>
+                </a>
+            </li>
+            <li class="header text-center">ACTIVO FIJO</li>
+            <li>
+                <a href="../activo_fijo/registro_activo.php">
+                    <i class="material-icons col-red">contacts</i>
+                    <span>Registro de Activo</span>
+                </a>
+            </li>
+            <li>
+                <a href="../activo_fijo/Lista.php">
+                    <i class="material-icons col-red">contacts</i>
+                    <span>Lista de Activo</span>
+                </a>
+            </li>
+            <li>
+                <a href="../activo_fijo/registro_tipo_activo.php">
+                    <i class="material-icons col-amber">mail</i>
+                    <span>Registro de Tipo de Activo</span>
+                </a>
+            </li>
+            <li>
+                <a href="../activo_fijo/registro_encargado.php">
+                    <i class="material-icons col-amber">mail</i>
+                    <span>Registro de Encargado</span>
+                </a>
+            </li>
+            <li>
+                <a href="../activo_fijo/registro_institucion.php">
+                    <i class="material-icons col-amber">mail</i>
+                    <span>Registro de Institucion</span>
+                </a>
+            </li>
+            <li>
+                <a href="../activo_fijo/registro_departamento.php">
+                    <i class="material-icons col-amber">mail</i>
+                    <span>Registro de Departamento</span>
+                </a>
+            </li>
+            
+        </ul>
+    </div>
+    <!-- #Menu -->
+    <!-- Footer -->
+    <div class="legal">
+        <div class="copyright">
+            © 2017 <a href="#">UES FMP.</a>
+        </div>
+
+    </div>
+    <!-- #Footer -->
+</aside>
+<!-- #END# Left Sidebar -->
