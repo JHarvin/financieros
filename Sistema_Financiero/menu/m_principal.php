42b7ae7b7842a05102f1d00b65cc71e2a851e507
marcosmartinez14021991@gmail.com <tusito14021991>
2019-01-21 14:23:31 -0600

    reportes
    harvin come palos panameños

--- Sistema_Financiero/menu/m_principal.php
+++ Sistema_Financiero/menu/m_principal.php
@@ -93,6 +93,42 @@
                </li>
 
 
+                <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Reportes
+              <span class="caret"></span></a>
+             <ul class="dropdown-menu">
+                              <li>
+                                <a href="modulos/reportes/cat.php"> Articulos por Categorias</a>
+                            </li>
+                             <li>
+                                <a href="modulos/reportes/report_costo.php"> Articulos por Almacen Costo</a>
+                            </li>
+                            <li>
+                                <a href="modulos/reportes/dep.php"> Articulos por Almacen Precios Venta</a>
+                            </li>
+                            <li>
+                                <a href="modulos/reportes/compra_dia.php"> Compras de Articulos por Fecha</a>
+                            </li>
+                             <li>
+                                <a href="modulos/reportes/compra_cat.php"> Compras de Articulos por Categoria</a>
+                            </li>
+                            <li>
+                                <a href="modulos/reportes/compra_gen.php"> Compras General de Articulos</a>
+                            </li>
+                             <li>
+                                <a href="modulos/reportes/venta_dia.php"> Ventas de Articulos por fecha</a>
+                            </li>
+                            <li>
+                                <a href="modulos/reportes/venta_cat.php"> Ventas de Articulos por Categoria</a>
+                            </li>
+                            <li>
+                                <a href="modulos/reportes/venta_gen.php"> Ventas General de Articulos</a>
+                            </li>
+              </ul>
+          </li>
+
+
+
                <li>
                 <a href="../Cuentas-Activo-master/home/home_usuario.php" class="btn btn-danger">Ir a activo fijo</a>
 
